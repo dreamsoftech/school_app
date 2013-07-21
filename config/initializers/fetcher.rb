7798fde782a416a13e23e7cab398408127fc86a1
Kevin Reed <reedkevin88@gmail.com>
2013-07-22 00:00:26 +0800

    comment mailman

--- config/initializers/fetcher.rb
+++ config/initializers/fetcher.rb
@@ -1,26 +1,26 @@
 
 #!/usr/bin/env ruby
-require "rubygems"
-require "bundler/setup"
-require "mailman"
+# require "rubygems"
+# require "bundler/setup"
+# require "mailman"
 
-#Mailman.config.logger = Logger.new("log/mailman.log")  # uncomment this if you can want to #create a log file
-Mailman.config.poll_interval = 3  # change this number as per your needs. Default is 60 seconds
-Mailman.config.imap = {
-  server: 'imap.gmail.com', port: 993, ssl: true,
-  enable_starttls_auto: true,
-  authentication: 'plain',
-  username: 'reedkevin88@gmail.com',
-  password: 'fanatic8822'
-}
+# #Mailman.config.logger = Logger.new("log/mailman.log")  # uncomment this if you can want to #create a log file
+# Mailman.config.poll_interval = 30  # change this number as per your needs. Default is 60 seconds
+# Mailman.config.imap = {
+#   server: 'imap.gmail.com', port: 993, ssl: true,
+#   enable_starttls_auto: true,
+#   authentication: 'plain',
+#   username: 'reedkevin88@gmail.com',
+#   password: 'fanatic8822'
+# }
 
-Mailman::Application.run do
-  default do
-    begin
-      TaskMailer.receive(message)
-    rescue Exception => e
-      Mailman.logger.error "Exception occurred while receiving message:n#{message}"
-      Mailman.logger.error [e, *e.backtrace].join("n")
-    end
-  end
-end
+# Mailman::Application.run do
+#   default do
+#     begin
+#       TaskMailer.receive(message)
+#     rescue Exception => e
+#       Mailman.logger.error "Exception occurred while receiving message:n#{message}"
+#       Mailman.logger.error [e, *e.backtrace].join("n")
+#     end
+#   end
+# end
